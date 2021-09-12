53823b60ffb7b2bd602449fd01dbfdd261278386
Jeroen <jeroen.demeyst@gmail.com>
2021-09-12 21:22:05 +0200

    C3 - wrong commit in the main class

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,7 +1,7 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
         if(args.length == 0)
-            System.out.println("No args shared");
+            System.out.println("No args shared -- wrong git commit addition");
         else if (args.length > 0)
             System.out.println("You shared : " + args.length + " args.");
     }
