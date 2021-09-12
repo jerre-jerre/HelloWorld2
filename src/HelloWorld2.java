9c9ab02cafb88ec761996f4b8295ba7c68d6392b
Jeroen <jeroen.demeyst@gmail.com>
2021-09-12 21:13:45 +0200

    C1 - all incl too much

---
+++ src/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        if(args.length == 0)
+            System.out.println("No args shared");
+        else if (args.length > 0)
+            System.out.println("You shared : " + args.length + " args.");
+    }
+}
