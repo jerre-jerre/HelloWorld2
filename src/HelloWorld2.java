60e889026ee7b3c4164aaf032d6257b957033788
Jeroen <jeroen.demeyst@gmail.com>
2021-09-12 21:23:13 +0200

    Revert "C3 - wrong commit in the main class"
    This reverts commit 53823b60ffb7b2bd602449fd01dbfdd261278386.

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,7 +1,7 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
         if(args.length == 0)
-            System.out.println("No args shared -- wrong git commit addition");
+            System.out.println("No args shared");
         else if (args.length > 0)
             System.out.println("You shared : " + args.length + " args.");
     }
